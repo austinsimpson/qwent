758083dd9c05920aff8476934697dbe649bf7dce
austinsimpson <austin.simpson@me.com>
2020-11-29 13:53:53 -0800

    Added missing files from last commit

---
+++ IQwentStrategy.h
@@ -0,0 +1,13 @@
+#ifndef IQWENTSTRATEGY_H
+#define IQWENTSTRATEGY_H
+
+class QwentGame;
+
+class IQwentStrategy
+{
+public:
+	virtual void performTurn(QwentGame* game) = 0;
+	virtual ~IQwentStrategy() = default;
+};
+
+#endif // IQWENTSTRATEGY_H
